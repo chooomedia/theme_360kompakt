ab8ea05f71cf4fe3c9ff68ab7d5ce4a6e5c01543
Rene <rene@lightweb-media.de>
2023-02-27 20:44:01 +0000

    Anzeige des Autors in Archiven gefixt (Vertikal Align center) + Verlinkung NUR auf Name

--- template-parts/author-info.php
+++ template-parts/author-info.php
@@ -17,16 +17,17 @@
 
 
          <img alt="<?php echo sprintf(__('Autorenfoto von %s', 'kompakt'), $author_name) ?>" title="<?php echo sprintf(__('Autorenfoto von %s', 'kompakt'), $author_name) ?>" src="<?php echo get_avatar_url($author_id) ?>" />
-         <?php if ($link) {?>
-         <?php echo __('Von ', 'kompakt');?>
-         
-         <a title="<?php echo sprintf(__('Autorenprofil von %s', 'kompakt'), $author_name) ?>" href="<?php echo get_author_posts_url($author_id) ?> ">
-         <?php  } ?>
-         
-         <?php echo $author_name ?>
 
-         <?php if ($link) {
-                echo '</a>';
-            }
-            ?>
+         <?php echo __('Von ', 'kompakt');?>
+
+         <?php if ($link) {?>
+
+         <a title="<?php echo sprintf(__('Autorenprofil von %s', 'kompakt'), $author_name) ?>" href="<?php echo get_author_posts_url($author_id) ?> ">
+         <?php echo $author_name ?>
+         <?php echo '</a>' ?>
+
+         <?php  } else { ?>
+         
+         <?php echo $author_name;
+         } ?>
  </div>
